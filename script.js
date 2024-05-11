b5fa188e40f55bb45e65247ee9856daf0d97b484
Christian Isaman <christianisaman@outlook.com>
2024-05-11 12:10:07 +0800

    init

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log(
+    "A HaiKu is a three line poem.\n\n" +
+    "It's supposed to be about nature, so mine isn't very good, but beauty is in the eyes of the beholder.\n\n" +
+    "Email me your own HaiKu!"
+);
